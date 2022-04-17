212d46e47e430f9919793d9692352eb70ac78e2c
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-18 00:59:07 +0600

    Card update
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -31,64 +31,66 @@ export default function Sidebar() {
 
   return (
     <Box flex={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      <List
-        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-        component='nav'
-        aria-labelledby='nested-list-subheader'
-        subheader={
-          <ListSubheader component='div' id='nested-list-subheader'>
-            Dashboard
-          </ListSubheader>
-        }
-      >
-        <ListItem disablePadding>
-          <ListItemButton component='a' href='#home'>
-            <ListItemIcon>
-              <Home />
-            </ListItemIcon>
-            <ListItemText primary='Homepage' />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton component='a' href='#simple-list'>
-            <ListItemIcon>
-              <Settings />
-            </ListItemIcon>
-            <ListItemText primary='Settings' />
-          </ListItemButton>
-        </ListItem>
-
-        <ListItem disablePadding>
-          <ListItemButton onClick={handleClick}>
-            <ListItemIcon>
-              <Inbox />
-            </ListItemIcon>
-            <ListItemText primary='Inbox' />
-            {open ? <ExpandLess /> : <ExpandMore />}
-          </ListItemButton>
-        </ListItem>
-
-        <Collapse in={open} timeout='auto' unmountOnExit>
-          <List component='div' disablePadding>
-            <ListItemButton sx={{ pl: 4 }}>
+      <Box sx={{ position: 'fixed' }}>
+        <List
+          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+          component='nav'
+          aria-labelledby='nested-list-subheader'
+          subheader={
+            <ListSubheader component='div' id='nested-list-subheader'>
+              Dashboard
+            </ListSubheader>
+          }
+        >
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
               <ListItemIcon>
-                <StarBorder />
+                <Home />
               </ListItemIcon>
-              <ListItemText primary='Starred' />
+              <ListItemText primary='Homepage' />
             </ListItemButton>
-          </List>
-        </Collapse>
+          </ListItem>
 
-        <ListItem disablePadding>
-          <ListItemButton>
-            <ListItemIcon>
-              <DarkMode />
-            </ListItemIcon>
-            <Switch />
-          </ListItemButton>
-        </ListItem>
-      </List>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#simple-list'>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary='Settings' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton onClick={handleClick}>
+              <ListItemIcon>
+                <Inbox />
+              </ListItemIcon>
+              <ListItemText primary='Inbox' />
+              {open ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+          </ListItem>
+
+          <Collapse in={open} timeout='auto' unmountOnExit>
+            <List component='div' disablePadding>
+              <ListItemButton sx={{ pl: 4 }}>
+                <ListItemIcon>
+                  <StarBorder />
+                </ListItemIcon>
+                <ListItemText primary='Starred' />
+              </ListItemButton>
+            </List>
+          </Collapse>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DarkMode />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 }
