b35125a72edb1a32386e680427fa9bb2864af38e
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-17 20:48:49 +0600

    Add sidebar menu
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,94 @@
-import { Box } from '@mui/material';
-import React from 'react';
+import {
+  DarkMode,
+  Drafts,
+  ExpandLess,
+  ExpandMore,
+  Home,
+  Inbox,
+  Send,
+  Settings,
+  StarBorder,
+} from '@mui/icons-material';
+import {
+  Box,
+  Collapse,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Switch,
+} from '@mui/material';
+import React, { useState } from 'react';
 
 export default function Sidebar() {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
-    <Box
-      bgcolor='lightcoral'
-      flex={1}
-      p={2}
-      sx={{ display: { xs: 'none', sm: 'block' } }}
-    >
-      Sidebar
+    <Box flex={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <List
+        sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
+        component='nav'
+        aria-labelledby='nested-list-subheader'
+        subheader={
+          <ListSubheader component='div' id='nested-list-subheader'>
+            Dashboard
+          </ListSubheader>
+        }
+      >
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#home'>
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary='Homepage' />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component='a' href='#simple-list'>
+            <ListItemIcon>
+              <Settings />
+            </ListItemIcon>
+            <ListItemText primary='Settings' />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton onClick={handleClick}>
+            <ListItemIcon>
+              <Inbox />
+            </ListItemIcon>
+            <ListItemText primary='Inbox' />
+            {open ? <ExpandLess /> : <ExpandMore />}
+          </ListItemButton>
+        </ListItem>
+
+        <Collapse in={open} timeout='auto' unmountOnExit>
+          <List component='div' disablePadding>
+            <ListItemButton sx={{ pl: 4 }}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary='Starred' />
+            </ListItemButton>
+          </List>
+        </Collapse>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <DarkMode />
+            </ListItemIcon>
+            <Switch />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 }
