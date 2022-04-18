d98c985ac38a8af945a84100e65ce458ca4e1426
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-18 10:34:56 +0600

    Card update finish
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,15 +1,16 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export default function Rightbar() {
   return (
     <Box
-      bgcolor='skyblue'
       flex={2}
       p={2}
       sx={{ display: { xs: 'none', sm: 'block' } }}
     >
-      Rightbar
+      <Box sx={{ position: 'fixed' }}>
+        <Typography variant='h6' fontWeight={100}>Online Friends</Typography>
+      </Box>
     </Box>
   );
 }
