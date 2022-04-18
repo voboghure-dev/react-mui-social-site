2016969d472374d405abaef7bfd80423364a8304
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-18 12:26:44 +0600

    Mui Tooltip, Fab, Icons
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Box, Stack } from '@mui/material';
+import Add from './components/Add';
 // import { Box } from '@mui/system';
 import Feed from './components/Feed';
 import Navbar from './components/Navbar';
@@ -14,6 +15,7 @@ function App() {
         <Feed />
         <Rightbar />
       </Stack>
+      <Add />
     </Box>
   );
 }
---
+++ src/components/Add.jsx
@@ -0,0 +1,22 @@
+import { Fab, Tooltip } from '@mui/material';
+import React from 'react';
+import { Add as AddIcon } from '@mui/icons-material';
+
+export default function Add() {
+  return (
+    <>
+      <Tooltip
+        title='Delete'
+        sx={{
+          position: 'fixed',
+          bottom: 20,
+          left: { xs: 'calc(50% - 25px)', md: 30 },
+        }}
+      >
+        <Fab color='primary' aria-label='add'>
+          <AddIcon />
+        </Fab>
+      </Tooltip>
+    </>
+  );
+}
