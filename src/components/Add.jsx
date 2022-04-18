5bc1b6198e2e61eed38a94894c708a9de09c7298
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-04-18 13:05:12 +0600

    MUI Modal
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,22 +1,103 @@
-import { Fab, Tooltip } from '@mui/material';
-import React from 'react';
-import { Add as AddIcon } from '@mui/icons-material';
+import {
+  Avatar,
+  Box,
+  Button,
+  ButtonGroup,
+  Fab,
+  Modal,
+  Stack,
+  TextField,
+  Tooltip,
+  Typography,
+} from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Add as AddIcon,
+  DateRange,
+  EmojiEmotions,
+  Image,
+  PersonAdd,
+  VideoCameraBack,
+} from '@mui/icons-material';
+import styled from '@emotion/styled';
+
+const StyledModal = styled(Modal)({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const StyledUserBox = styled(Box)({
+  display: 'flex',
+  alignItems: 'center',
+  gap: '10px',
+  marginBottom: '20px',
+});
 
 export default function Add() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <Tooltip
+        onClick={handleOpen}
         title='Delete'
         sx={{
           position: 'fixed',
           bottom: 20,
           left: { xs: 'calc(50% - 25px)', md: 30 },
+          cursor: 'pointer',
         }}
       >
         <Fab color='primary' aria-label='add'>
           <AddIcon />
         </Fab>
       </Tooltip>
+      <StyledModal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby='modal-modal-title'
+        aria-describedby='modal-modal-description'
+      >
+        <Box width={400} height={280} bgcolor='white' p={3} borderRadius={5}>
+          <Typography variant='h6' color='gray' textAlign='center'>
+            Create Post
+          </Typography>
+          <StyledUserBox>
+            <Avatar
+              sx={{ height: 30, width: 30 }}
+              src='https://images.pexels.com/photos/941693/pexels-photo-941693.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+            />
+            <Typography>John Doe</Typography>
+          </StyledUserBox>
+          <TextField
+            sx={{ width: '100%' }}
+            id='standard-multiline-static'
+            multiline
+            rows={4}
+            placeholder="What's on your mind"
+            variant='standard'
+          />
+          <Stack direction='row' gap={1} mt={2} mb={3}>
+            <EmojiEmotions color='primary' />
+            <Image color='secondary' />
+            <VideoCameraBack color='success' />
+            <PersonAdd color='error' />
+          </Stack>
+          <ButtonGroup
+            fullWidth
+            variant='contained'
+            aria-label='outlined primary button group'
+          >
+            <Button>Post</Button>
+            <Button sx={{ width: '100px' }}>
+              <DateRange />
+            </Button>
+          </ButtonGroup>
+        </Box>
+      </StyledModal>
     </>
   );
 }
